2783286259988ae2bc0d13045d1d6fc16938379c
barle <barlegoopl@gmail.com>
2026-05-29 02:33:36 +0200

    Wstępny zarys porgramu na podstawie lab

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+# taxi zarys imie, nazwisko, lokalizacja, numer telefonu, znajomosć języka, profilowe, auto rodzaj, marka, kolor, rejestracja
+# użytkownik, imię, lokalizacja, numer telefonu
 users= [
     {"Name":"Artur","Location": "Warszawa", "Posts": ["Sprzedam Mercedessa","Kupię skrzynie biegów","ratunku co robić po wypadku"]},
     {"Name":"Daniel","Location": "Legionowo", "Posts": ["Mój kod nie działa"]},
